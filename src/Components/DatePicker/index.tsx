ce6c871cf5eb6ec2adf0e2ac6361dc97040b9b29
bryanfernandesFrwk <bryanfernandes@frwk.com.br>
2023-02-18 17:00:07 -0300

    deploy

--- src/Components/DatePicker/index.tsx
+++ src/Components/DatePicker/index.tsx
@@ -6,20 +6,5 @@ import { Calendar, CalendarProps } from 'primereact/calendar';
 export default function DatePicker(props: CalendarProps) {
     return <div>
         <Calendar {...props} />
-        <style jsx='true' >{`
-        .p-datepicker-calendar{
-            /* color: ${colors.primary[6]}; */
-        }
-        .p-datepicker .p-datepicker-header .p-datepicker-title .p-datepicker-year, .p-datepicker .p-datepicker-header .p-datepicker-title .p-datepicker-month{
-            // color: ${colors.primary[5]};
-        }
-        .p-datepicker .p-datepicker-header .p-datepicker-prev, .p-datepicker .p-datepicker-header .p-datepicker-next{
-            // color: ${colors.primary[5]};
-        }
-        .p-button.p-button-secondary.p-button-text, .p-buttonset.p-button-secondary > .p-button.p-button-text, .p-splitbutton.p-button-secondary > .p-button.p-button-text, .p-fileupload-choose.p-button-secondary.p-button-text{
-            // color: ${colors.primary[5]};
-
-        }
-        `}</style>
     </div>
 }
