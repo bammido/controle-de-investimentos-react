69ae1b399da689c17acb658f374db5eb2758b8a4
bryanfernandesFrwk <bryanfernandes@frwk.com.br>
2023-02-18 16:50:06 -0300

    tratativas de erro decryptAES e cryptoAES

--- src/helpers/functions/decryptAES.ts
+++ src/helpers/functions/decryptAES.ts
@@ -1,9 +1,18 @@
 import CryptoJS from 'crypto-js';
 
 export default function decryptAES(cryptoToken: string) {
-    const decryptedToken = CryptoJS.AES.decrypt(
-        cryptoToken,
-        'secret key 751'
-    ).toString(CryptoJS.enc.Utf8)
-    return JSON.parse(decryptedToken)
+    try {
+        const decryptedToken = CryptoJS.AES.decrypt(
+            cryptoToken,
+            'secret key 751'
+        ).toString(CryptoJS.enc.Utf8)
+
+        if (!decryptedToken) {
+            throw new Error('falha ao descriptografar')
+        }
+
+        return JSON.parse(decryptedToken)
+    } catch (error) {
+        console.error(error)
+    }
 }
--- src/helpers/functions/setTokenLocal.ts
+++ src/helpers/functions/setTokenLocal.ts
@@ -3,5 +3,5 @@ import cryptoAES from './cryptoAES';
 export default function setTokenLocal(token: string) {
     const cryptoToken = cryptoAES(token)
 
-    return localStorage.setItem('usuario_token', cryptoToken)
+    return localStorage.setItem('usuario_token', cryptoToken as string)
 }
